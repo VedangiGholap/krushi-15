150817dd6afc24b789fc5ba4235d0088216d95a1
VedangiGholap <vedangi.gh3110@gmail.com>
2025-04-20 08:57:27 +0530

    cloud firestore imported

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:krushi_version16/pages/customer/home.dart'; // Make sure this path is correct
 import 'package:krushi_version16/pages/farmer/farmer_home.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; //cloud firestore db
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
